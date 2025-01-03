82086e888a63d1612867486a7421f66d7c4a9335
ritik <iamritiknagar@gmail.com>
2025-01-04 01:47:17 +0530

    portfolio

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Hero.css'
+// import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+
+const Hero = () => {
+  return (
+    <div className='hero' id='home'>
+       <img src="src/assets/rn.jpg" alt="" /> 
+       <h1><span>I'm  Ritik Nagar,</span> frontend developer  based in India.</h1>  
+       <p>I am frontend developer,from Rajasthan, I'm looking for new opportunity 'MERN STACK DEVELOPER' and passionate developer. </p>
+       <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+        </div>  
+
+    </div>
+  )
+}
+
+export default Hero
